fdea94d3a15fdd81a9159226379f34e9097fd517
jld3103 <jld3103yt@gmail.com>
2023-06-18 22:11:25 +0200

    Update to 27

--- pkg/common/constants.go
+++ pkg/common/constants.go
@@ -1,8 +1,8 @@
 //nolint:gochecknoglobals
 package common
 
-var VersionString = "26.0.0"
-var VersionMajor = int64(26)
+var VersionString = "27.0.0"
+var VersionMajor = int64(27)
 var VersionMinor = int64(0)
 var VersionMicro = int64(0)
 var Edition = "rain-cloud"
